2c6e00a6e0fb6453e60f7d3af11e8f458b9434f4
Luis Jover <luisjoverdev@gmail.com>
2024-02-22 02:27:35 +0100

    feat(api): create and refactor api calls to match updated needs

--- src/api/drinks.ts
+++ src/api/drinks.ts
@@ -5,26 +5,29 @@ import { DetailedDrinkResponseData, DrinkByIngredientListData } from "../types/D
 
 //GET ALL DRINKS BY INGREDIENT
 
-export const getAllDrinksByIngredient = async (ingredient: string) => {
+export const getAllDrinksByIngredient = async (ingredient: string): Promise<DrinkByIngredientListData> => {
 
     const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`
     try {
         const response: AxiosResponse<DrinkByIngredientListData> = await axios.get(endpoint);
-        return response
+        return response.data
     } catch (error) {
         console.log(error);
+        throw error;
     }
 }
 
 
+//GET DETAIL DRINK BY ID
 
-export const getDrinkById = async (drinkId: string) => {
+export const getDrinkById = async (drinkId: string): Promise<DetailedDrinkResponseData> => {
 
     const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`
     try {
         const response: AxiosResponse<DetailedDrinkResponseData> = await axios.get(endpoint);
-        return response
+        return response.data
     } catch (error) {
         console.log(error);
+        throw error
     }
 }
