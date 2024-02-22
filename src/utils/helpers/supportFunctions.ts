f0857277c107bf5f777c29f82e2ab5688953a149
Luis Jover <luisjoverdev@gmail.com>
2024-02-22 02:28:42 +0100

    feat(checck and  order): create and refactor functions to check ingredients number and order by alcohol content

--- src/utils/helpers/supportFunctions.ts
+++ src/utils/helpers/supportFunctions.ts
@@ -1,6 +1,20 @@
+
+import { AppDrinkData, DetailedDrinkData } from "../../types/DrinkData";
+
+
 // Function to check if a drink has 6 or fewer ingredients
-const has6OrLessIngredients = (drinkData: any) => {
-    const ingredientKeys = Object.keys(drinkData).filter((key) => key.startsWith("strIngredient") && drinkData[key] !== null);
-    return ingredientKeys.length <= 6;
+export const hasSixOrLessIngredients = (drink: DetailedDrinkData) => {
+    // Verify strIngredient7 is null
+    return drink.strIngredient7 === null;
+};
+
+
+//Function to order drinks array by alcohol content
+export const orderDrinksByAlcohol = (drinks: AppDrinkData[]) => {
+    const nonAlcoholic = drinks.filter(drink => drink.strAlcoholic === "Non alcoholic");
+    const optionalAlcohol = drinks.filter(drink => drink.strAlcoholic === "Optional alcohol");
+    const alcoholic = drinks.filter(drink => drink.strAlcoholic === "Alcoholic");
+
+    return [...nonAlcoholic, ...optionalAlcohol, ...alcoholic];
 };
 
